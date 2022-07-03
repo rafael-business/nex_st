92ac302492a628722b3a0622c56ff6d84a3f1d64
Rafael dos Santos Pedro <rspwebdesigner@gmail.com>
2022-07-03 03:47:27 -0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 32,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+});
+
+/*
+var vantagens = new Swiper(".vantagens", {
+    slidesPerView: "auto",
+    centeredSlides: false,
+    spaceBetween: 32,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+});
+*/
