00ef5af65cb1c10db4b2da9cc002b9e938c8b1b3
Rafael dos Santos Pedro <rspwebdesigner@gmail.com>
2022-07-17 19:57:11 -0300

    Desenvolvimento

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,38 @@
-var swiper = new Swiper(".mySwiper", {
+"use strict";
+
+var swiper = new Swiper(".maps", {
     slidesPerView: "auto",
     centeredSlides: true,
-    spaceBetween: 32,
+    autoplay: {
+        delay: 4500,
+    },
     scrollbar: {
         el: ".swiper-scrollbar",
         hide: true,
     },
 });
 
-/*
-var vantagens = new Swiper(".vantagens", {
-    slidesPerView: "auto",
-    centeredSlides: false,
-    spaceBetween: 32,
-    scrollbar: {
-        el: ".swiper-scrollbar",
-        hide: true,
-    },
+$( function(){
+
+    const spinner = $( '#form_submit .fa-spinner' );
+    const form = $( '.form-contato' );
+    $( '#form_submit' ).on( 'click', function(e){
+
+        e.preventDefault();
+        spinner.toggleClass( 'hide' );
+    
+        $.ajax({
+            type: "POST",
+            url: form.attr( 'action' ),
+            data: form.serialize(),
+            crossDomain: true,
+            dataType: 'jsonp',
+            encode: true,
+        }).
+        done( function( data ){
+            
+            console.log(data);
+        });
+    });
+  
 });
-*/
